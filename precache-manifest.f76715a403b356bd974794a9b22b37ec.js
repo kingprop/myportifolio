9d35d2795f993f87e397ad6a09c04952bd3c3eaa
kingprop <highnessking94@gmail.com>
2019-07-08 23:28:50 +0300

    Updates

---
+++ precache-manifest.f76715a403b356bd974794a9b22b37ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea144c374d1ac8672e61a2a59c2bfd1",
+    "url": "/yportifolio/index.html"
+  },
+  {
+    "revision": "c6b3185111f164452eed",
+    "url": "/yportifolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "ae7720096d4b3914153a",
+    "url": "/yportifolio/static/css/main.f63df90c.chunk.css"
+  },
+  {
+    "revision": "c6b3185111f164452eed",
+    "url": "/yportifolio/static/js/2.59ce4812.chunk.js"
+  },
+  {
+    "revision": "ae7720096d4b3914153a",
+    "url": "/yportifolio/static/js/main.aff4641f.chunk.js"
+  },
+  {
+    "revision": "e287e4e1201fbf634698",
+    "url": "/yportifolio/static/js/runtime~main.d41bbc38.js"
+  }
+]);
